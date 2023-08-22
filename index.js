31672e0da0d3be3cbf71d7f4f732e3c6de1b2160
Ngai Tik Chung <NgaitikK.Chung94@gmail.com>
2023-08-21 23:28:29 -0400

    index.html

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Your code here
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'ArrowLeft') {
+      moveDodgerLeft();
+    } else if (e.key === 'ArrowRight') {
+      moveDodgerRight();
+    }
+  });
+
+  
+  function moveDodgerLeft() {
+    const dodger = document.getElementById('dodger');
+    const left = parseInt(dodger.style.left) || 0;
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  
+  
+
+  function moveDodgerRight() {
+    const dodger = document.getElementById('dodger');
+    const left = parseInt(dodger.style.left) || 0;
+  
+    if (left + 40 <= 400) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+  
